10b8ba537f7f891239768e4ce9cd1a38cb3d17c4
unknown <xiangzhichan@126.com>
2015-10-02 23:04:14 +0800

    changes log

---
+++ ZipAndUnzip/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ZipAndUnzip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Zip = new System.Windows.Forms.Button();
+            this.mkTBox = new System.Windows.Forms.MaskedTextBox();
+            this.Unzip = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Zip
+            // 
+            this.Zip.Location = new System.Drawing.Point(44, 201);
+            this.Zip.Name = "Zip";
+            this.Zip.Size = new System.Drawing.Size(80, 28);
+            this.Zip.TabIndex = 0;
+            this.Zip.Text = "压缩";
+            this.Zip.UseVisualStyleBackColor = true;
+            this.Zip.Click += new System.EventHandler(this.Zip_Click);
+            // 
+            // mkTBox
+            // 
+            this.mkTBox.Location = new System.Drawing.Point(91, 91);
+            this.mkTBox.Name = "mkTBox";
+            this.mkTBox.Size = new System.Drawing.Size(140, 21);
+            this.mkTBox.TabIndex = 1;
+            // 
+            // Unzip
+            // 
+            this.Unzip.Location = new System.Drawing.Point(197, 204);
+            this.Unzip.Name = "Unzip";
+            this.Unzip.Size = new System.Drawing.Size(80, 25);
+            this.Unzip.TabIndex = 2;
+            this.Unzip.Text = "解压";
+            this.Unzip.UseVisualStyleBackColor = true;
+            this.Unzip.Click += new System.EventHandler(this.Unzip_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(321, 300);
+            this.Controls.Add(this.Unzip);
+            this.Controls.Add(this.mkTBox);
+            this.Controls.Add(this.Zip);
+            this.Name = "Form1";
+            this.Text = "ZipAndUnzip";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Zip;
+        private System.Windows.Forms.MaskedTextBox mkTBox;
+        private System.Windows.Forms.Button Unzip;
+    }
+}
+
---
+++ ZipAndUnzip/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.Zip; 
+
+namespace ZipAndUnzip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Zip_Click(object sender, EventArgs e)
+        {
+            string srcFile = @"..\..\..\..\text.txt";//准备压缩的文件路径
+            string zipFile = @"..\..\..\..\testzip";//压缩后的文件路径
+            Console.WriteLine("使用BZIP开始压缩文件……");
+            if (BZipFile(srcFile, zipFile + ".bz"))//使用BZIP压缩文件
+            {
+                MessageBox.Show("文件压缩成功");
+                //Console.WriteLine("文件压缩完成");
+            }
+            else
+            {
+                Console.WriteLine("文件压缩失败");
+            }
+        }
+        private bool BZipFile(string sourcefilename, string zipfilename)
+        {
+            bool blResult;//表示压缩是否成功的返回结果
+            //为源文件创建文件流实例，作为压缩方法的输入流参数
+            FileStream srcFile = File.OpenRead(sourcefilename);
+            //为压缩文件创建文件流实例，作为压缩方法的输出流参数
+            FileStream zipFile = File.Open(zipfilename, FileMode.Create);
+            try
+            {
+                //以4096字节作为一个块的方式压缩文件
+                BZip2.Compress(srcFile, zipFile, 4096);
+                blResult=true;
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.Message);
+                blResult=false;
+            }
+            srcFile.Close();//关闭源文件流
+            zipFile.Close();//关闭压缩文件流
+            return blResult;
+        }
+        //文件解压
+        public void UnZip(string zipedFile, string strDirectory, string password, bool overWrite)
+        {
+            if (strDirectory == "")
+                strDirectory = Directory.GetCurrentDirectory();
+
+            if (!strDirectory.EndsWith("\\"))
+                strDirectory = strDirectory + "\\";
+
+            using (ZipInputStream s = new ZipInputStream(File.OpenRead(zipedFile)))
+            {
+                s.Password = password;
+                ZipEntry theEntry;
+
+                while ((theEntry = s.GetNextEntry()) != null)
+                {
+                    string directoryName = "";
+                    string pathToZip = "";
+                    pathToZip = theEntry.Name;
+
+                    if (pathToZip != "")
+                        directoryName = Path.GetDirectoryName(pathToZip) + "\\";
+
+                    string fileName = Path.GetFileName(pathToZip);
+
+                    Directory.CreateDirectory(strDirectory + directoryName);
+                    if (fileName != "")
+                    {
+                        if ((File.Exists(strDirectory + directoryName + fileName) && overWrite) || (!File.Exists(strDirectory + directoryName + fileName)))
+                        {
+                            using (FileStream streamWriter = File.Create(strDirectory + directoryName + fileName))
+                            {
+                                int size = 2048;
+                                byte[] data = new byte[2048];
+                                while (true)
+                                {
+                                    size = s.Read(data, 0, data.Length);
+                                    if (size > 0)
+                                        streamWriter.Write(data, 0, size);
+                                    else
+                                        break;
+                                }
+                                streamWriter.Close();
+                            }
+                        }
+                    }
+                }
+                s.Close();
+            }
+        }
+
+        private void Unzip_Click(object sender, EventArgs e)
+        {
+            UnZip("hksnzt.zip","E:\\","123",true);
+        }  
+    }
+}
